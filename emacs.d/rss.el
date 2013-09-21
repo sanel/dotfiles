14ac9eff3507c226396124eeddc61ef8b8d15259
Sanel Zukan <karijes@users.sourceforge.net>
2013-09-22 00:03:35 +0200

    Updating...

--- emacs.d/rss.el
+++ emacs.d/rss.el
@@ -8,17 +8,26 @@
 
 (setq newsticker-html-renderer 'lynx-renderer)
 
+
+(set-face-foreground 'newsticker-treeview-face "gray")
+(set-face-foreground 'newsticker-treeview-face "gray")
+(set-face-foreground 'newsticker-treeview-selection-face "black")
+(set-face-foreground 'newsticker-treeview-old-face "gray")
+
 (setq newsticker-keep-obsolete-items nil
 	  newsticker-scroll-smoothly     nil
 	  newsticker-use-full-width      nil
 	  newsticker-sort-method         'sort-by-time
 	  newsticker-retrieval-interval  600
 	  newsticker-treeview-treewindow-width 40
-	  newsticker-treeview-listwindow-height 10)
+	  newsticker-treeview-listwindow-height 10
+	  newsticker-automatically-mark-items-as-old t
+	  newsticker-automatically-mark-visited-items-as-old t)
 
 (setq newsticker-url-list-defaults
 	  '(("Emacs Wiki" "http://www.emacswiki.org/cgi-bin/wiki.pl?action=rss" nil 3600)
 		("Slashdot" "http://slashdot.org/slashdot.rss" nil 3600)
+		("LWN" "http://lwn.net/headlines/newrss" nil 3600)
 		("Hacker News" "http://news.ycombinator.com/rss" nil 3600)
 		("Reddit Programming" "http://www.reddit.com/r/programming/.rss" nil 3600)
 		("Reddit Scheme" "http://www.reddit.com/r/scheme/.rss" nil 3600)
